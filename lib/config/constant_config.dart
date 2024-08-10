1280bde035d00086dfb6c773f0d3753892571462
Elmer Matthew Japara <elmer.japara@next-ti.co.id>
2024-08-10 14:48:41 +0700

    Add more scenarios for integration tests and widget test

--- lib/config/constant_config.dart
+++ lib/config/constant_config.dart
@@ -3,6 +3,6 @@ import 'dart:io';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ConstantConfig {
-  // final String keyNewsApi = dotenv.env['NEWS_API_KEY'] ?? '';
-  final String keyNewsApi = Platform.environment['NEWS_API_KEY'] ?? '';
+  // final String keyNewsApi = dotenv.env['NEWS_API_KEY'] ?? ''; //uncomment this line when you want to debug locally
+  final String keyNewsApi = Platform.environment['NEWS_API_KEY'] ?? ''; //uncomment this line when you want to use with CI/CD
 }
